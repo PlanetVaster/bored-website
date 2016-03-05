534fbd04ad57d7b6365dedeb4e36a4d82a64574d
Andrew Mitchell <planetvaster@gmail.com>
2016-03-05 15:43:33 -0500

    Load facts from a text file

--- js/button.js
+++ js/button.js
@@ -1,8 +1,20 @@
 //Wait for page to load
-window.onload = function() {
+window.onload = function () {
     
     //Get a reference to the link with an id of "bored_button_link"
     var link = document.getElementById("bored_button_link");
+    var factsFile;
+    var facts = [null];
+    
+    var file = new XMLHttpRequest();
+    file.open('GET', 'resources/facts.txt');
+    
+    file.onreadystatechange = function() {
+        factsFile = file.responseText;
+        getLinesFromTextFile(factsFile);
+    }
+    
+    file.send();
     
     //Called when the link is clicked
     link.onclick = function() {
@@ -29,7 +41,7 @@ window.onload = function() {
     
     function sayCoolFact()
     {
-        var totalNumberOfFacts = 6;
+        var totalNumberOfFacts = 7;
         var numberOfFacts = Math.floor((Math.random() * totalNumberOfFacts) + 1);
         
         switch (numberOfFacts)
@@ -39,32 +51,50 @@ window.onload = function() {
                 document.getElementById("cool-fact").innerText = "Oh noes! something went wrong! [Cool Fact]";
                 break;
             case 1:
-                document.getElementById("cool-fact").innerText = "In 2006 an Australian man tried to sell New Zealand on eBay. The price reached " + 
-                "$3,000 before eBay shut it down!";
+                document.getElementById("cool-fact").innerText = facts[1];
+                //document.getElementById("cool-fact").innerText = "In 2006 an Australian man tried to sell New Zealand on eBay. The price reached " + 
+                //"$3,000 before eBay shut it down!";
                 break;
             case 2:
-                document.getElementById("cool-fact").innerText = "In Japan, letting a sumo wrestler make your baby cry is considered good luck.";
+                document.getElementById("cool-fact").innerText = facts[2];
+                //document.getElementById("cool-fact").innerText = "In Japan, letting a sumo wrestler make your baby cry is considered good luck.";
                 break;
             case 3:
-                document.getElementById("cool-fact").innerText = "It would take at least 480 bananas to die of potassium overdose.";
+                document.getElementById("cool-fact").innerText = facts[3];
+                //document.getElementById("cool-fact").innerText = "It would take at least 480 bananas to die of potassium overdose.";
                 break;
             case 4:
-                document.getElementById("cool-fact").innerText = "Nutella was invented during WWII, when an Italian pastry maker mixed hazelnuts " +
-                "into chocolate to extend his chocolate ration.";
+                document.getElementById("cool-fact").innerText = facts[4];
+                //document.getElementById("cool-fact").innerText = "Nutella was invented during WWII, when an Italian pastry maker mixed hazelnuts " +
+                //"into chocolate to extend his chocolate ration.";
                 break;
             case 5:
-                document.getElementById("cool-fact").innerText = "The present population is expected to rise to 8 billion by 2020, 10 billion by 2050, and 15 " +
-                "billion by 2080!";
+                document.getElementById("cool-fact").innerText = facts[5];
+                //document.getElementById("cool-fact").innerText = "The present population is expected to rise to 8 billion by 2020, 10 billion by 2050, and 15 " +
+                //"billion by 2080!";
                 break;
             case 6:
+                document.getElementById("cool-fact").innerText = facts[6];
+                //document.getElementById("cool-fact").innerText = "The tip of a bullwhip moves so fast the sound you hear" +
+                //" is actually a tiny sonic boom!";
+            case 7:
                 document.getElementById("title-of-activity").innerText = "Cool Website";
                 document.getElementById("cool-fact").innerHTML = "At <a style=\"color:aqua; margin: 0; padding: 0;\"" +
                 "href=\"https://nthitz.github.io/turndownforwhatjs/\" id=\"cool-fact\" target=\"_blank\">" +
                 "this site</a> you can make any website turn down.";
-                
                 break;
         }
         
         return false;
+    }
+    
+    function getLinesFromTextFile(file)
+    {
+        var lines = file.split("\n");
+        
+        for (var i = 0; i < lines.length; i++)
+        {
+            facts[i + 1] = lines[i];
+        }
     }
 }
